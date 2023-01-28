cac4784d446d6105a65be5c04cf1111590aab96d
Shreeram Kumar Kushwaha <117209741+kshreeram813@users.noreply.github.com>
2023-01-28 16:40:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+ <script>
+        function onsignup(event)
+        {
+            event.preventDefault();
+            console.log(event.target.username.value);
+            console.log(event.target.email.value);
+            console.log(event.target.phone1.value);
+            console.log(event.target.date1.value);
+            console.log(event.target.time1.value);
+        }
+    </script>
